313e2fb2470942380ff9dbec6f69e0201b9714d4
Daun <147286216+Tegarrn@users.noreply.github.com>
2025-03-17 19:21:14 +0700

    Update Player.jsx

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -211,11 +211,12 @@ export default function Player({
   useEffect(() => {
     if (!streamUrl || !artRef.current) return;
     const iframeUrl = streamInfo?.streamingLink?.iframe;
-    console.log(iframeUrl);
     const headers = {};
     if (iframeUrl) {
       const url = new URL(iframeUrl);
       headers.Referer = url.origin + "/";
+    }else{
+      headers.Referer = "https://megacloud.club/";
     }
     const art = new Artplayer({
       url:
@@ -492,10 +493,14 @@ export default function Player({
         JSON.parse(localStorage.getItem("continueWatching")) || [];
 
       const newEntry = {
+        id: animeInfo?.id,
         data_id: animeInfo?.data_id,
         episodeId,
         episodeNum,
-        animeInfo,
+        adultContent: animeInfo?.adultContent,
+        poster: animeInfo?.poster,
+        title: animeInfo?.title,
+        japanese_title: animeInfo?.japanese_title,
       };
       if (!newEntry.data_id) return;
       const existingIndex = continueWatching.findIndex(
